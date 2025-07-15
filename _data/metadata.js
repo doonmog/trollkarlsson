d81cbf74560a0d45038fbf4a9f98907e814ebcf5
moondog-matthew <94130507+moondog-matthew@users.noreply.github.com>
2025-07-15 14:30:40 +0200

    Set metadata, clean up garbage

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "Trollkarlssons Blog",
+	url: "https://trollkarlsson.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Portfolio.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Markus Karlsson",
+		email: "markus@trollkarlsson.com",
+		url: "https://trollkarlsson.com/about-me/"
 	}
 }
